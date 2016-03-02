8ab2ba4c37266daa80f7fd963a3b53f0751c688e
haikentcode <hiteshnitj16@gmail.com>
2016-03-02 21:34:16 +0530

    webhaios work

--- webhaios/home/views.py
+++ webhaios/home/views.py
@@ -34,40 +34,26 @@ import cv2
 def handle_uploaded_file(f,name):
     ext=str(name.split(".").pop())
     name=str(time.time()).replace(".","")
-    with open('media/'+name+"."+ext, 'wb+') as destination:
+    imagePath='media/'+name+"."+ext
+    with open(imagePath, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    return imagePath
 
+def getSimilarImages(imagePath): # pass image path to get list of similar images path and urls
+       imagesList=[]
+       for i in range(100):
+           imagesList.append(imagePath)
+       return imagesList
 
 @csrf_exempt
 def upload(request):
     f=request.FILES['file']
     name=f.name
-    handle_uploaded_file(f,name)
-    return HttpResponse("ho gya tera")
-
-
-@csrf_exempt
-def calculate(request):
-    expr=request.POST.get("expr")
-    r=eval(expr)
-    return HttpResponse(r)
-
-def gettime(request):
-    t=str(datetime.datetime.utcnow()).split(".")[0]
-    return HttpResponse(t)
-
-@csrf_exempt
-def StudentForm(request):
-    if request.method == 'POST':
-        form=Student(request.POST,request.FILES)
-        if form.is_valid():
-            form.save()
-            print "save"
-        else:
-            print request.FILES.get('image')
-    studentform=Student()
-    return render(request,'home/student.html',{"form":studentform})
+    imagePath=handle_uploaded_file(f,name)
+    print imagePath
+    similarImagesList=getSimilarImages(imagePath)
+    return render(request,'home/imageslist.html',{"similarImagesList":similarImagesList}) #get templet of list of images
 
 
 def index(request):
@@ -75,7 +61,7 @@ def index(request):
         form = FeedBack(request.POST)
         if form.is_valid():
            print request.POST.get('topic')
-           return render(request,'home/index.html',{"name":"india","feedBackForm":form})
+           return render(request,'home/index.html',{"name":"HAIOS","feedBackForm":form})
     print request
     feedBackForm=FeedBack()
-    return render(request,'home/index.html',{"name":"india","feedBackForm":feedBackForm})
+    return render(request,'home/index.html',{"name":"HAIOS","feedBackForm":feedBackForm})
